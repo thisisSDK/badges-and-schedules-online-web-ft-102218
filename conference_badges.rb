69e0817e37a0bbe723724a94c4601a0a6f47de3f
sdk1270 <sdk1270@gmail.com>
2018-10-24 16:22:35 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,6 +16,12 @@ def assign_rooms(attendees)
   end
 end
 
-def printer
+def printer(attendees)
+  batch_badge_creator(attendees).each do |assignment|
+    puts badge
+  end
+  assign_rooms(attendees).each do |assignment|
+    puts assignment
+  end
 end
 
