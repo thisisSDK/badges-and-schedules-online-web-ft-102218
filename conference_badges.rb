da4f7b1d037abf79879dcf4b8514104a17929b8b
sdk1270 <sdk1270@gmail.com>
2018-10-24 16:24:01 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,7 @@ def batch_badge_creator(attendees)
 end 
 
 def assign_rooms(attendees)
-  attendes.each_with_index do |attendee, index|
+  attendes.map.with_index do |attendee, index|
     "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
   end
 end
